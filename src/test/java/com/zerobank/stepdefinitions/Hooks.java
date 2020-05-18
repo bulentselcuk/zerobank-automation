4bf9a2e182c736f8c54a8651689f928620871eea
bulentselcuk <bulentselcuk@gmail.com>
2020-05-17 19:35:30 -0500

    zerobank

--- src/test/java/com/zerobank/stepdefinitions/Hooks.java
+++ src/test/java/com/zerobank/stepdefinitions/Hooks.java
@@ -1,29 +1,32 @@
 package com.zerobank.stepdefinitions;
 
 import com.zerobank.utilities.Driver;
-import com.zerobank.utilities.Driver;
-import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
     @Before
-    public void setUp(){
-        Driver.get().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        Driver.get().manage().window().maximize();
+    public void setUp() {
+        Driver.get ().manage ().timeouts ().implicitlyWait ( 5, TimeUnit.SECONDS );
+        Driver.get ().manage ().window ().maximize ();
     }
 
     @After
-    public void tearDown(Scenario scenario){
-        //if the scenario fails take the screenshot
-        if(scenario.isFailed()){
-            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
-            scenario.embed(screenshot,"image/png");
-        }
+
+    public void tearDown() {
         Driver.closeDriver();
     }
-
 }
+
+
+//    public void tearDown(Scenario scenario){
+//        //if the scenario fails take the screenshot
+//        if(scenario.isFailed()){
+//            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+//            scenario.embed(screenshot,"image/png");
+//        }
+//        Driver.closeDriver();
+//    }
+//
+//}
